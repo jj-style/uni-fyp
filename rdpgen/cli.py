6a448c359e7ad40e5e4aa2f69909de267c6a5e55
JJ Style <style.jj@pm.me>
2022-02-22 17:08:44 +0000

    add cli options for languauge options

--- rdpgen/cli.py
+++ rdpgen/cli.py
@@ -12,13 +12,37 @@ from .bnfparse.parsergen import parser_from_grammar
 @click.argument(
     "language", type=click.Choice(["c++", "go", "python"], case_sensitive=False)
 )
-def cli(file: str, outdir: str, language: str):
+@click.option(
+    "--case",
+    "-c",
+    type=click.Choice(["camel", "snake"]),
+    help="which case identifiers should be generated in",
+)
+@click.option(
+    "--expand-tabs/--no-expand-tabs",
+    help="expand tabs to spaces (default expand-tabs)",
+    default=True,
+)
+@click.option(
+    "--tab-size",
+    "-t",
+    type=int,
+    help="How many spaces in each layer of indentation (if expand-tabs is set)",
+)
+def cli(
+    file: str, outdir: str, language: str, case: str, expand_tabs: bool, tab_size: int
+):
     # parse config
     with open(file, "rb") as f:
         config = tomli.load(f)
 
     tokens = tokens_from_config_map(config.get("tokens", {}).items())
-    lang_opts = config.get("language_options", {})
+
+    lang_opts_cfg = config.get("language_options", {})
+    lang_opts_cli = {"case": case, "expand_tabs": expand_tabs, "tab_size": tab_size}
+    lang_opts_cli = {k: v for k, v in lang_opts_cli.items() if v is not None}
+    # cli args take precedence over config file
+    lang_opts = {**lang_opts_cfg, **lang_opts_cli}
 
     # create a lexer program
     template_lex_file(tokens, outdir)
